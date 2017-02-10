5b3090aa2409d9bfc57d6bb3802e462043c37909
Gian Darren Azriel Aquino <gda.i.aquino@gmail.com>
2017-02-10 17:47:34 +0800

    refactor

--- GoogleAuthenticator.js
+++ GoogleAuthenticator.js
@@ -8,47 +8,51 @@ var TOKEN_PATH = 'client_token.json';
 var SECRET_PATH = 'client_secret.json';
 var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 
-/** Request access token from google **/
 function authorize(done) {
     async.waterfall([
-        function(callback) {
-            _readFile(SECRET_PATH, function(err, result) {
-                if (err) return callback(err);
-                return callback(null, JSON.parse(result));
-            });
-        },
-        function(secret, callback) {
-            _readFile(TOKEN_PATH, function(err, result) {
-                if (err) {
-                    return callback(null, secret, null);
+            /** load client_secret.json **/
+            function(callback) {
+                _readFile(SECRET_PATH, function(err, result) {
+                    if (err) return callback(err);
+                    return callback(null, JSON.parse(result));
+                });
+            },
+            /** load client_token.json **/
+            function(secret, callback) {
+                _readFile(TOKEN_PATH, function(err, result) {
+                    if (err) return callback(null, secret, null);
+                    return callback(null, secret, JSON.parse(result));
+                });
+            },
+            /** create oauth and request token if null **/
+            function(secret, token, callback) {
+                var oauth = _createOAuth(secret);
+                if (token) {
+                    oauth.credentials = token;
+                    return callback(null, oauth);
                 }
-                return callback(null, secret, JSON.parse(result));
-            });
-        },
-        function(secret, token, callback) {
-            var auth = new googleAuth();
-            var client_id = secret.installed.client_id;
-            var client_secret = secret.installed.client_secret;
-            var redirect = secret.installed.redirect_uris[0];
-            var oauth = new auth.OAuth2(client_id, client_secret, redirect);
-            if (token) {
-                oauth.credentials = token;
-                return callback(null, oauth);
+                _requestToken(oauth, function(err, result) {
+                    if (err) return callback(err);
+                    oauth.credentials = result;
+                    _writeFile(TOKEN_PATH, JSON.stringify(result));
+                    return callback(null, oauth);
+                })
             }
-            _requestToken(oauth, function(err, result) {
-                if (err) return callback(err);
-                oauth.credentials = result;
-                _writeFile(TOKEN_PATH, JSON.stringify(result));
-                return callback(null, oauth);
-            })
-        }
         ],
         function(err, result) {
             return done(err, result);
         });
 }
 
+function _createOAuth(secret) {
+    var auth = new googleAuth();
+    var oauth = new auth.OAuth2(secret.installed.client_id,
+        secret.installed.client_secret,
+        secret.installed.redirect_uris[0]);
+    return oauth;
+}
 
+/** request token via command line io **/
 function _requestToken(oauth, callback) {
     var url = oauth.generateAuthUrl({
         access_type: 'offline',
